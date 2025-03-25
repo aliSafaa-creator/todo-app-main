900517220697208a4cf4ff83f6f8cb92d9795494
ali safaa <alisafaaa33@gmail.com>
2025-03-25 19:14:29 +0300

    Add filter functionality for todos and enhance attribution styling

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ const inputTodo = document.querySelector(".input-todo input");
 const todosContainer = document.querySelector(".todos");
 const itemsLeft = document.querySelector(".items-left");
 
+// Select filter buttons
+const filterAll = document.querySelector("[data-all]");
+const filterActive = document.querySelector("[data-active]");
+const filterCompleted = document.querySelector("[data-completed");
+
 // Add event listener for the theme toggle
 themeToggleIcon.addEventListener("click", themeToggle);
 
@@ -27,7 +32,6 @@ document.addEventListener("DOMContentLoaded", () => {
           const uncheckedTodos = todosContainer.querySelectorAll(
                ".todo input[type='checkbox']:not(:checked)"
           ).length;
-          console.log(uncheckedTodos);
 
           itemsLeft.textContent = `${uncheckedTodos} item${
                uncheckedTodos !== 1 && uncheckedTodos !== 0 ? "s" : ""
@@ -78,4 +82,42 @@ document.addEventListener("DOMContentLoaded", () => {
 
      // Initial update for items left
      updateItemsLeft();
+
+     // Filter functions
+     const filterTodos = (filter) => {
+          const todos = todosContainer.querySelectorAll(".todo");
+
+          todos.forEach((todo) => {
+               const checkbox = todo.querySelector("input[type='checkbox']");
+               switch (filter) {
+                    case "all":
+                         todo.style.display = "flex";
+                         break;
+
+                    case "active":
+                         if (checkbox.checked) {
+                              todo.style.display = "none";
+                         } else {
+                              todo.style.display = "flex";
+                         }
+                         break;
+
+                    case "completed":
+                         if (!checkbox.checked) {
+                              todo.style.display = "none";
+                         } else {
+                              todo.style.display = "flex";
+                         }
+                         break;
+
+                    default:
+                         todo.style.display = "flex";
+               }
+          });
+     };
+
+     // Add event listeners for filters
+     filterAll.addEventListener("click", () => filterTodos("all"));
+     filterActive.addEventListener("click", () => filterTodos("active"));
+     filterCompleted.addEventListener("click", () => filterTodos("completed"));
 });
